36fcb06096c3f6e657602e8620dd06ce81212b91
Wasim Akhtar <Wasim.Akhtar@concentrix.com>
2019-05-17 20:54:41 +0530

    up

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,8 +1,4 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ConsoleApp1
 {
@@ -10,6 +6,7 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+
             // The code provided will print ‘Hello World’ to the console.
             // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
             Console.WriteLine("Hello World!");
